0ded6447f1ecda5f7b85cc5cab56420803a7fe33
defosu-dev <rikyamorhun@gmail.com>
2026-01-15 19:33:31 +0300

    refactor(code): remove remnants of old implementation

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './app/App.tsx'
 import { StoreProvider } from './app/providers/StoreProvides.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <StoreProvider>
-        <App />
-      </StoreProvider>
-    </QueryClientProvider>
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </StrictMode>
 )
